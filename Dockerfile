c2d25d446bb138330e7d01006ea0651816afbf21
ManmathX <manmath2006n@gmail.com>
2026-04-26 10:24:04 +0530

    added all required changes

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM node:20-alpine AS frontend-builder
+
+
+
+COPY frontend/package*.json ./
+RUN npm install
+COPY frontend/ ./
+RUN npm run build
+
+
+FROM node:20-alpine AS backend
+WORKDIR /app/backend
+
+
+COPY backend/package*.json ./
+RUN npm install
+
+
+RUN npm install -g pm2
+
+
+COPY backend/ ./
+
+
+COPY --from=frontend-builder /app/frontend/dist /app/frontend/dist
+
+
+EXPOSE 80
+
+
+CMD ["pm2-runtime", "server.js"]
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const path = require("path");
-
+const path = require("path"); 
 const connectDB = require("./config/db");
-
 const userRoutes = require("./routes/userRoutes");
-
 dotenv.config();
 connectDB();
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+ res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`);
+})
